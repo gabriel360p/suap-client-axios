1a75b1aad8a04a578c1d91ba80c748a00ee62f0d
lorium17 <109006007+gabriel360p@users.noreply.github.com>
2023-10-16 09:23:11 -0300

    suap-vue

---
+++ teste1/suap1/public/js/settings.js
@@ -0,0 +1,4 @@
+var CLIENT_ID = '0y6KfWBgwN2S1ViJCLcVSxuaIKaSjg50ALfMru5D';
+var REDIRECT_URI = 'http://localhost:8080/';
+var SUAP_URL = 'https://suap.ifrn.edu.br';
+var SCOPE = 'identificacao email documentos_pessoais';
---
+++ teste1/suap1/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+
+  <div class="container">
+
+    <div>
+      <div>
+        <img alt="Vue logo" src="./assets/logo.png">
+      </div>
+      <div>
+        {{ user.nome_usual}}
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  },
+
+  data() {
+    return {
+      user: {}
+    }
+  },
+
+  mounted () {
+    this.dataUser()
+  },
+
+  methods: {
+    dataUser() {
+        //recuperando os dados do usuário
+        this.user=JSON.parse(localStorage.getItem("user"))
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
